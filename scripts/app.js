51924fd38edae5e8dc2e22df13230ad6ebff6961
Matthew Sun <matthewkol186@gmail.com>
2014-10-30 23:07:45 -0400

    Adds functioning slider with some really random weird annoying width changing issue on one of the h1 elements that can only be fixed by using a polling service. Very annoying.

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,83 @@
-var app = angular
-	.module('LHSWeb', [
-		'ui.router'
-	])
-	.config(function($urlRouterProvider, $stateProvider){
-		$urlRouterProvider.otherwise('/');
+	var app = angular
+		.module('LHSWeb', [
+			'ui.router'
+		])
+		.directive('titleImage', function(){
+			return{
 
-		$stateProvider
-			.state('home', {
-				url: '/',
-				templateUrl: 'views/home.html'
-			})
-			.state('about', {
-				url: '/about',
-				templateUrl: 'views/about.html'
-			})
-	})
+			}
+		})
+		.directive('captionImage', function(){
+			return{
+
+			}
+		})
+		.controller('CarouselCtrl', function($scope){
+			$scope.$on('$viewContentLoaded', function() {
+				var $window = $(window).on('resize', function(){
+			       var height = $(this).height() - $('#topnavbar').height();
+			       $('.photo-carousel > div').height(.7*height);
+			       $('.caption-image').height($('.title-image').height());
+		           $('.caption-image').outerWidth($('.photo-carousel').outerWidth() - $('.title-image').outerWidth());
+			    }).trigger('resize');
+				
+				$(".title-image").resize(function(e){
+					console.log($('.title-image > h1').width() + "whee");	
+					console.log($('.title-image > h1').css( "display" ));
+					$('.caption-image').outerWidth($('.photo-carousel').outerWidth() - $('.title-image').outerWidth());
+				});
+
+		        $('.photo-carousel').slick({
+		            //autoplay: true,
+		            //autoplaySpeed: 5000
+		        });
+		        $('.title-image').css({
+					"background-color": "#FFD700",
+					 "padding": "10px",
+					 "display": "inline",
+					 "float": "left"
+				});
+
+		        $('.caption-image').css({
+					"padding": "10px",
+					"margin": "0px",
+					"background-color": "rgba(96, 33, 87, 0.75)",
+					"display": "inline",
+					"float": "left"
+				});
+
+				$('.title-image > h1').css({
+					"padding": "0px",
+					"margin": "0px",
+					"display": "inline-block",
+					"float": "left"
+				});
+
+				console.log($('.title-image > h1').width() + "whee");
+				console.log($('.title-image > h1').css( "display" ));
+
+		        $('.caption-image').height($('.title-image').height());
+		        $('.caption-image').outerWidth($('.photo-carousel').outerWidth() - $('.title-image').outerWidth());
+		        $(window).load(function(){
+		        	$('.caption-image').height($('.title-image').height());
+		        	$('.caption-image').outerWidth($('.photo-carousel').outerWidth() - $('.title-image').outerWidth());
+		        	console.log($('.title-image').outerWidth());
+		        });
+			});
+
+
+		})
+		.config(function($urlRouterProvider, $stateProvider){
+			$urlRouterProvider.otherwise('/');
+
+			$stateProvider
+				.state('home', {
+					url: '/',
+					templateUrl: 'views/home.html',
+					controller: 'CarouselCtrl'
+				})
+				.state('about', {
+					url: '/about',
+					templateUrl: 'views/about.html'
+				})
+		})
