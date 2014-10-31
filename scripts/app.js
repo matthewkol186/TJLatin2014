31eadd9f304eb1d80eb47478188960a4cfc2dfbd
Matthew Sun <matthewkol186@gmail.com>
2014-10-31 00:52:32 -0400

    A wonderful state of things with a beautiful slider and updated navbar.

--- scripts/app.js
+++ scripts/app.js
@@ -2,67 +2,18 @@
 		.module('LHSWeb', [
 			'ui.router'
 		])
-		.directive('titleImage', function(){
-			return{
-
-			}
-		})
-		.directive('captionImage', function(){
-			return{
-
-			}
-		})
 		.controller('CarouselCtrl', function($scope){
 			$scope.$on('$viewContentLoaded', function() {
 				var $window = $(window).on('resize', function(){
 			       var height = $(this).height() - $('#topnavbar').height();
 			       $('.photo-carousel > div').height(.7*height);
-			       //$('.caption-image').height($('.title-image').height());
-		           //$('.caption-image').outerWidth($('.photo-carousel').outerWidth() - $('.title-image').outerWidth());
 			    }).trigger('resize');
-				
-				/*$(".title-image").resize(function(e){
-					console.log($('.title-image > h1').width() + "whee");	
-					console.log($('.title-image > h1').css( "display" ));
-					$('.caption-image').outerWidth($('.photo-carousel').outerWidth() - $('.title-image').outerWidth());
-				});*/
 
 		        $('.photo-carousel').slick({
-		            //autoplay: true,
-		            //autoplaySpeed: 5000
+		            autoplay: true,
+		            autoplaySpeed: 5000,
+		            dots: true
 		        });
-		        /*$('.title-image').css({
-					"background-color": "#FFD700",
-					 "padding": "10px",
-					 "display": "inline",
-					 "float": "left"
-				});
-
-		        $('.caption-image').css({
-					"padding": "10px",
-					"margin": "0px",
-					"background-color": "rgba(96, 33, 87, 0.75)",
-					"display": "inline",
-					"float": "left"
-				});
-
-				$('.title-image > h1').css({
-					"padding": "0px",
-					"margin": "0px",
-					"display": "inline-block",
-					"float": "left"
-				});
-
-				console.log($('.title-image > h1').width() + "whee");
-				console.log($('.title-image > h1').css( "display" ));
-
-		        $('.caption-image').height($('.title-image').height());
-		        $('.caption-image').outerWidth($('.photo-carousel').outerWidth() - $('.title-image').outerWidth());
-		        $(window).load(function(){
-		        	$('.caption-image').height($('.title-image').height());
-		        	$('.caption-image').outerWidth($('.photo-carousel').outerWidth() - $('.title-image').outerWidth());
-		        	console.log($('.title-image').outerWidth());
-		        });*/
 			});
 
 
@@ -73,7 +24,7 @@
 			$stateProvider
 				.state('home', {
 					url: '/',
-					templateUrl: 'views/hometest.html',
+					templateUrl: 'views/home.html',
 					controller: 'CarouselCtrl'
 				})
 				.state('about', {
